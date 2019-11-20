d03dd8b9d19cc29e4adee24c788f8928d95f03ed
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-20 21:45:31 +0100

    First Implementation

---
+++ Assets/Scripts/Cavalier.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+public class Cavalier : MonoBehaviour
+{
+	public CavalierData CavalierData;
+	private int Live;
+
+
+}
---
+++ Assets/Scripts/Data/CavalierData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CavalierData : ScriptableObject
+{ 
+	public int MaxLive = 100;
+	public float MaxSpeed = 5;
+	//
+}
