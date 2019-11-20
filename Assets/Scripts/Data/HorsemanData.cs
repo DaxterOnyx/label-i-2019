8a1eb86f7785a6a3190ca228dc3b6a7ffd5431af
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-21 00:38:51 +0100

    Import packages

---
+++ Assets/Scripts/Data/HorsemanData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "HorsemenData", menuName = "Data / HorsemenData")]
+public class HorsemanData : ScriptableObject
+{
+	public int MaxHealthPoint = 100;
+	public float SpeedWalking = 5;
+	public float SpeedRiding = 10;
+
+	//
+}
